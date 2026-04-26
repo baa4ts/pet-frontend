2aba3900795704d15d507e08e5fe7c0da34e4794
baa4ts <baa4tsdev@gmail.com>
2026-04-25 22:16:05 -0300

    Mejora la mescla

---
+++ src/components/shared/Pagination.tsx
@@ -0,0 +1,50 @@
+import { useCallback } from "react";
+import { useSearchParams } from "react-router";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+interface Props {
+  total: number;
+}
+
+export const Pagination = ({ total }: Props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const page = Number(searchParams.get("page") || "1");
+  const limit = searchParams.get("limit") || "15";
+  const totalPages = Math.ceil(total / Number(limit));
+
+  const onPrev = useCallback(() => {
+    if (page <= 1) return;
+    setSearchParams((prev) => {
+      prev.set("page", String(page - 1));
+      return prev;
+    });
+  }, [page, setSearchParams]);
+
+  const onNext = useCallback(() => {
+    if (page >= totalPages) return;
+    setSearchParams((prev) => {
+      prev.set("page", String(page + 1));
+      return prev;
+    });
+  }, [page, totalPages, setSearchParams]);
+
+  return (
+    <article className="flex h-12 flex-row items-center">
+      <div className="flex h-full items-center mx-4">
+        Total de elementos {` ${total}`}
+      </div>
+      <Separator orientation="vertical" />
+      <div className="flex h-full items-center gap-2 mx-4">
+        <Button onClick={onPrev} disabled={page <= 1}>Anterior</Button>
+        <div className="w-16 flex items-center justify-center">
+          {total > 0 ? `${page} / ${totalPages}` : "0 / 0"}
+        </div>
+        <Button onClick={onNext} disabled={page >= totalPages}>Siguiente</Button>
+      </div>
+      <Separator orientation="vertical" />
+    </article>
+  );
+};
--- src/paginas/Administracion/paginas/DashNoticias.tsx
+++ src/paginas/Administracion/paginas/DashNoticias.tsx
@@ -1,5 +1,18 @@
+import { Pagination } from "@/components/shared/Pagination";
+import { Separator } from "@/components/ui/separator";
+
 const DashNoticias = () => {
-  return <>1</>;
+  return (
+    <section className="flex flex-1 flex-col overflow-hidden">
+      <article className="flex-7 min-h-0">
+        <div className="h-full flex flex-col overflow-y-auto">
+          Noticias
+        </div>
+      </article>
+      <Separator />
+      <Pagination total={230} />
+    </section>
+  );
 };
 
 export default DashNoticias;
